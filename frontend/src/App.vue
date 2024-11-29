dbca32237ae926091e55dc6b9072750ad753994d
Dennis Kühnlein (Dev) <d.kuehnlein@outlook.com>
2024-11-29 23:35:45 +0100

    Navbar with logo, welcome-picture and excluding media-queries for now

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,37 +5,20 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img id="logo"/>
-    <p id="logo_name">ShutterSpecht</p>
+    <div id="header">
+      <img id="logo" src="./assets/Logo.png" alt="Logo ShutterSpecht"/>
+      <p id="logo_name">ShutterSpecht</p>
+    </div>
+    <div id="welcome_picture">
+      <!--<img id="welcome_picture_wedding" src="./assets/pictures/wedding.jpg" alt="Welcome-Picture Wedding"/>-->
+    </div>
   </header>
+  <div id="service_content">
+    
+  </div>
 
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
