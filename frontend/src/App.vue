341bd6108d7c9cca1076123fed92e661dd961265
Dennis <d.kuehnlein@outlook.com>
2024-11-30 14:41:00 +0100

    Navbar, service-part and service_boxen implemented

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,13 +9,29 @@ import TheWelcome from './components/TheWelcome.vue'
       <img id="logo" src="./assets/Logo.png" alt="Logo ShutterSpecht"/>
       <p id="logo_name">ShutterSpecht</p>
     </div>
-    <div id="welcome_picture">
-      <!--<img id="welcome_picture_wedding" src="./assets/pictures/wedding.jpg" alt="Welcome-Picture Wedding"/>-->
-    </div>
   </header>
-  <div id="service_content">
-    
-  </div>
+  <section id="service_content">
+    <div id="service">
+      <div class="container">
+        <h2 id="heading_service">Service ist alles</h2>
+        
+        <div class="flexbox">
+          <div class="service_box">
+            <img class="service_box_image" src="./assets/pictures/wedding.jpg" alt="Image 1 Service"/>
+            <h4 class="service_box_heading">Wedding</h4>
+          </div>
+          <div class="service_box">
+            <img class="service_box_image" src="./assets/pictures/events.jpg" alt="Image 2 Service"/>
+            <h4 class="service_box_heading">Events</h4>
+          </div>
+          <div class="service_box">
+            <img class="service_box_image" src="./assets/pictures/personal.jpg" alt="Image 3 Service"/>
+            <h4 class="service_box_heading">Personal</h4>
+          </div>
+        </div>
+      </div> 
+    </div>
+  </section>
 
 </template>
 
