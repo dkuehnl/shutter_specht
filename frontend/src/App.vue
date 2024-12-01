c77b5ff253367aaf1a1c2dd01be3cbca71e8d105
Dennis Kühnlein (Dev) <d.kuehnlein@outlook.com>
2024-12-01 20:23:26 +0100

    finished welcome- and service-part, start with event-section

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -17,19 +17,33 @@ import TheWelcome from './components/TheWelcome.vue'
         
         <div class="flexbox">
           <div class="service_box">
-            <img class="service_box_image" src="./assets/pictures/wedding.jpg" alt="Image 1 Service"/>
+            <img class="service_box_image image__weeding" src="./assets/pictures/wedding.jpg" alt="Image 1 Service"/>
             <h4 class="service_box_heading">Wedding</h4>
           </div>
           <div class="service_box">
-            <img class="service_box_image" src="./assets/pictures/events.jpg" alt="Image 2 Service"/>
+            <img class="service_box_image image__events" src="./assets/pictures/events.jpg" alt="Image 2 Service"/>
             <h4 class="service_box_heading">Events</h4>
           </div>
           <div class="service_box">
-            <img class="service_box_image" src="./assets/pictures/personal.jpg" alt="Image 3 Service"/>
+            <img class="service_box_image image__personal" src="./assets/pictures/personal.jpg" alt="Image 3 Service"/>
             <h4 class="service_box_heading">Personal</h4>
           </div>
         </div>
       </div> 
+    </div>
+  </section>
+
+  <section id="event_content">
+    <div class="container">
+      <div class="event_description">
+        <h2 class="heading_event">
+          <span class="heading_event heading_small">Der beste Ansprechpartner<br /></span>
+          Für jedes Event
+        </h2>
+        <p class="event_p">
+          
+        </p>
+      </div>
     </div>
   </section>
 
